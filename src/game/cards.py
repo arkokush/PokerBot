862b2e8758cb77ece4cf5c4fd7db8815dc6fe9f5
arkokush <kokush.arkady@gmail.com>
2025-11-13 10:15:34 -0500

    addded card.id

--- src/game/cards.py
+++ src/game/cards.py
@@ -1,27 +1,36 @@
 import random
 
 class Card:
-    def __init__(self, suit, value):
+    __slots__ = ['suit','rank','id']
+
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.value = value
+        self.rank = rank
+        rank_map = {
+            "2": 0, "3": 1, "4": 2, "5": 3, "6": 4, "7": 5, "8": 6, "9": 7,
+            "T": 8, "J": 9, "Q": 10, "K": 11, "A": 12,
+        }
+        suit_map = {
+            "C": 0, "D": 1, "H": 2, "S": 3,
+            "c": 0, "d": 1, "h": 2, "s": 3,
+        }
+        self.id = rank_map[rank]*4 + suit_map[suit]
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
 
 class Deck:
     def __init__(self):
         self.cards = []
-        for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]:
-            for value in range(2, 10):
-                self.cards.append(Card(suit, value))
-            for value in ["J", "Q", "K", "A"]:
-                self.cards.append(Card(suit, value))
+        for suit in ["H", "D", "C", "S"]:
+            for rank in ['2','3','4','5','6','7','8','9','T','J', 'Q', 'K', 'A']:
+                self.cards.append(Card(suit, rank))
 
     def shuffle(self):
         random.shuffle(self.cards)
 
     def deal(self, n):
-        dealt = []
-        for i in range(n):
-            dealt.append(self.cards.pop())
-        return dealt
+        return [self.cards.pop() for _ in range(n)]
 
     def size(self):
         return len(self.cards)
@@ -30,8 +39,7 @@ def main():
     deck = Deck()
     deck.shuffle()
     for card in deck.cards:
-        print(f"{card.value} of {card.suit}")
+        print(card)
 
 if __name__ == "__main__":
     main()
-
